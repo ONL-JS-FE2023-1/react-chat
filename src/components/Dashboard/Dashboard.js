2d9d59641594cee70094889a92ca99c1d637da86
Roman Ratushnyi <freshcode1001@ukr.net>
2023-08-04 19:20:13 +0300

    Chat 2/2

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,23 +5,18 @@ import MessageArea from '../MessageArea/MessageArea';
 import styles from './Dashboard.module.css';
 import { UserContext } from '../../contexts/userContext';
 import { getData } from '../../api';
+import { reducer } from '../../reducers';
 import CONSTANTS from '../../constants';
 const {ACTIONS} = CONSTANTS;
 
-
-function reducer(state, action) {
-
-}
-
 const Dashboard = () => {
     const [user, setUser] = useState({
-        id: 1,
         username: 'c123s',
-        imageSrc: 'https://robohash.org/c123s'
     })
 
     const [state, dispatch] = useReducer(reducer, {
-        messages: []
+        messages: [],
+        error: null
     })
 
     useEffect(() => {
@@ -36,18 +31,36 @@ const Dashboard = () => {
         })
         .catch((error) => {
             dispatch({
-                type: ACTIONS.DATA_LOAD_ERROR
+                type: ACTIONS.DATA_LOAD_ERROR,
+                payload: {
+                    error
+                }
             })
         })
     }, [])
+
+    const createMessage = (text) => {
+        const {messages} = state;
+        const newMessage = {
+            body: text,
+            id: messages.length + 1,
+            user
+        }
+        dispatch({
+            type: ACTIONS.ADD_NEW_MESSAGE,
+            payload: {
+                newMessage
+            }
+        })
+    }
 
     return (
         <UserContext.Provider value={user}>
             <main className={styles.container}>
             <DialogList /> {/* row */}
             <section className={styles.wrapper}>
-                <Chat /> {/* column */}
-                <MessageArea /> {/* column */}
+                <Chat dialog={state.messages} /> {/* column */}
+                <MessageArea addMessage={createMessage} /> {/* column */}
             </section>
         </main>
         </UserContext.Provider>
